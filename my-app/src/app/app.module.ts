929bbbbfb63c90d66ad515080120e2ed932f6dbf
Dashanga <kireytatyana@yandex.ru>
2019-10-25 20:23:06 +0200

    DifPages

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RaspisanieComponent } from './raspisanie/raspisanie.component';
+import { MainComponent } from './main/main.component';
+import { AdminComponent } from './admin/admin.component';
+
+const appRoutes: Routes = [
+{ path: 'main', component: MainComponent },
+{ path: 'raspisanie', component: RaspisanieComponent },
+{ path: 'admin', component: AdminComponent },
+{ path: '', redirectTo: '/main', pathMatch: 'full' }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+declarations: [
+AppComponent,
+RaspisanieComponent,
+MainComponent,
+AdminComponent
+],
+imports: [
+RouterModule.forRoot(
+appRoutes,
+{ enableTracing: true } // <— debugging purposes only
+),
+BrowserModule,
+AppRoutingModule
+],
+providers: [],
+bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ my-app/src/app/raspisanie/raspisanie.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-raspisanie',
+  templateUrl: './raspisanie.component.html',
+  styleUrls: ['./raspisanie.component.less']
+})
+export class RaspisanieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
